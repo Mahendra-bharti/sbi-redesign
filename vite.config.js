1f0adb32ca30d4b1525b7fb56c9527432a4c370e
Mahendra-bharti <mahendrabbharti2003@gmail.com>
2025-03-20 15:15:34 +0530

    Initial commit

--- vite.config.js
+++ vite.config.js
@@ -5,8 +5,4 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react() , tailwindcss(),],
-  base: '/sbi-clone/',  // ✅ Match your repo name exactly
-  build: {
-    outDir: 'dist'       // ✅ Ensure correct build directory
-  }
 })
